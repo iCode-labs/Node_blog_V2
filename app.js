4e6a6c7beef43de9f86640a810ce29e09909be9d
jdappengine <unknown>
2013-08-30 17:43:28 +0800

    var PORT

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ http.createServer(function (req, res) {
 <iframe border="0" frameborder="0" marginwidth="0" marginheight="0" width="100%" height="100%" src="http://man.jcloud.com/appengine/jae/hello.html"> </iframe> \
   ');
   res.end();
-}).listen(process.env.VMC_APP_PORT || 1337, null);
+}).listen(process.env.PORT || 1337, null);
